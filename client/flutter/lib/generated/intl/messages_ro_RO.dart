b32b3c1d567ea2a5d5718fd1666fd4772623cfb7
Claudiu Groza <groza.claudiu9217@gmail.com>
2020-03-28 13:21:03 +0200

    Add ro_RO localization resource

---
+++ client/flutter/lib/generated/intl/messages_ro_RO.dart
@@ -0,0 +1,73 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a ro_RO locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'ro_RO';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static _notInlinedMessages(_) => <String, Function> {
+    "commonWorldHealthOrganization" : MessageLookupByLibrary.simpleMessage("Organizația Mondială a Sănătății"),
+    "healthCheckTitle" : MessageLookupByLibrary.simpleMessage("Control medical"),
+    "homePagePageButtonLatestNumbers" : MessageLookupByLibrary.simpleMessage("Ultimele \nCifre"),
+    "homePagePageButtonProtectYourself" : MessageLookupByLibrary.simpleMessage("Protejează-te"),
+    "homePagePageButtonTravelAdvice" : MessageLookupByLibrary.simpleMessage("Sfaturi de Călătorie"),
+    "homePagePageButtonWHOMythBusters" : MessageLookupByLibrary.simpleMessage("OMS Mituri Desfiinţate"),
+    "homePagePageButtonYourQuestionsAnswered" : MessageLookupByLibrary.simpleMessage("Răspunsuri la \nÎntrebările \nTale"),
+    "homePagePageSliverListAboutTheApp" : MessageLookupByLibrary.simpleMessage("Despre aplicație"),
+    "homePagePageSliverListAboutTheAppDialog" : MessageLookupByLibrary.simpleMessage("Aplicația oficială a Organizației Mondiale a Sănătății pentru COVID-19."),
+    "homePagePageSliverListProvideFeedback" : MessageLookupByLibrary.simpleMessage("Oferă feedback despre aplicație"),
+    "homePagePageSliverListShareTheApp" : MessageLookupByLibrary.simpleMessage("Partajați aplicația"),
+    "protectYourselfListOfItemsPageListItem1" : MessageLookupByLibrary.simpleMessage("Spălați-vă des mâinile cu săpun și apă proaspătă"),
+    "protectYourselfListOfItemsPageListItem2" : MessageLookupByLibrary.simpleMessage("Evitați atingerea ochilor, gurii și a nasului"),
+    "protectYourselfListOfItemsPageListItem3" : MessageLookupByLibrary.simpleMessage("Acoperiți-vă gura și nasul cu cotul sau un șervețel atunci când tușiți sau strănutați"),
+    "protectYourselfListOfItemsPageListItem4" : MessageLookupByLibrary.simpleMessage("Evitați locurile aglomerate"),
+    "protectYourselfListOfItemsPageListItem5" : MessageLookupByLibrary.simpleMessage("Stați acasă dacă vă simțiți rău - chiar și cu febră sau tuse ușoară"),
+    "protectYourselfListOfItemsPageListItem6" : MessageLookupByLibrary.simpleMessage("Dacă aveți febră, tuse și dificultăți în a respira, căutați neîntârziat asistență medicală folosind în primă instanță telefonul!"),
+    "protectYourselfListOfItemsPageListItem7" : MessageLookupByLibrary.simpleMessage("Fiți la curent cu ultimele informații furnizate de OMS"),
+    "travelAdviceListOfItemsPageListItem1" : MessageLookupByLibrary.simpleMessage("OMS continuă să recomande neaplicarea unor contrângeri de călătorie sau afaceri pentru țările care se confruntă cu focare COVID-19…"),
+    "travelAdviceListOfItemsPageListItem10" : MessageLookupByLibrary.simpleMessage("Folosiți masca de protecție dacă sunteți bolnav cu simptome de COVID-19 (în special, tuse) sau aveți în grijă pe cineva care este suspect de COVID-19"),
+    "travelAdviceListOfItemsPageListItem11" : MessageLookupByLibrary.simpleMessage("Călătorii care se întorc din zonele afectate sunt obligați:"),
+    "travelAdviceListOfItemsPageListItem12" : MessageLookupByLibrary.simpleMessage("Auto-monitorizarea simptomelor pentru 14 zile și respectarea regulilor naționale în țările găzduitoare. Anumite țări solicită intrarea în carantină a călătorilor"),
+    "travelAdviceListOfItemsPageListItem13" : MessageLookupByLibrary.simpleMessage("Termoscannerele POT detecta dacă o personă are febră, dar NU POT detecta dacă persoana respectivă are sau nu coronavirus"),
+    "travelAdviceListOfItemsPageListItem14" : MessageLookupByLibrary.simpleMessage("Dacă simptome ca febra, tusea sau dificultatea în a respira își fac apariția, călătorii sunt sfătuiți să contacteze unitățile medicale locale, preferabil prin telefon, și să-i informeze despre simptome și itinerariu"),
+    "travelAdviceListOfItemsPageListItem2" : MessageLookupByLibrary.simpleMessage("Este prudent ca pasagerii care sunt bolnavi să amâne sau să evite calătoriile în zonele afectate, în special cei în vârstă și cu boli cronice sau stări precare de sănătate…"),
+    "travelAdviceListOfItemsPageListItem3" : MessageLookupByLibrary.simpleMessage("“Zone afectate” sunt considerate acele țări, provincii, teritorii sau orașe care suferă de transmiterea continuă a COVID-19, în constrast cu acele zone care comunică doar cazuri importate…"),
+    "travelAdviceListOfItemsPageListItem4" : MessageLookupByLibrary.simpleMessage("Recomandările generale pentru majoritatea pasagerilor includ…"),
+    "travelAdviceListOfItemsPageListItem5" : MessageLookupByLibrary.simpleMessage("Spălați-vă des mâinile"),
+    "travelAdviceListOfItemsPageListItem6" : MessageLookupByLibrary.simpleMessage("Evitați atingerea ochilor, gurii și a nasului"),
+    "travelAdviceListOfItemsPageListItem7" : MessageLookupByLibrary.simpleMessage("Acoperiți-vă gura și nasul cu cotul sau un șervețel atunci când tușiți sau strănutați"),
+    "travelAdviceListOfItemsPageListItem8" : MessageLookupByLibrary.simpleMessage("Respectați distanța de 1 metru (3 feet) de o persoană care este bolnavă"),
+    "travelAdviceListOfItemsPageListItem9" : MessageLookupByLibrary.simpleMessage("Respectați normele de igienă pentru mâncare"),
+    "whoMythBustersListOfItemsPageListItem1" : MessageLookupByLibrary.simpleMessage("Există o sumedenie de informații false în jurul nostru. Acestea sunt adevărurile"),
+    "whoMythBustersListOfItemsPageListItem10" : MessageLookupByLibrary.simpleMessage("Termoscannerele POT detecta dacă o personă are febră, dar NU POT detecta dacă persoana respectivă are sau nu coronavirus"),
+    "whoMythBustersListOfItemsPageListItem11" : MessageLookupByLibrary.simpleMessage("Pulverizarea de alcool sau clor peste suprafața corpului tău NU va omorî virușii care au pătruns deja în corp"),
+    "whoMythBustersListOfItemsPageListItem12" : MessageLookupByLibrary.simpleMessage("Vaccinurile împotriva pneumoniei, cum sunt vaccinul pneumococic și vaccinul Haemophilus influenzae de tip B, NU oferă protecție împotriva coronavirusului"),
+    "whoMythBustersListOfItemsPageListItem13" : MessageLookupByLibrary.simpleMessage("Nu există nicio dovadă că clătitul nasului cu soluție salină protejează persoanele împotriva infecției cu coronavirus"),
+    "whoMythBustersListOfItemsPageListItem14" : MessageLookupByLibrary.simpleMessage("Ustoroiul este sănătos, dar nu există nicio dovadă din epidemia curentă care să dovedească că consumul de usturoi te protejează împotriva coronavirusului"),
+    "whoMythBustersListOfItemsPageListItem15" : MessageLookupByLibrary.simpleMessage("Antibioticele NU funcționează împotriva virușilor, ele funcționeaza doar împotriva bacteriilor"),
+    "whoMythBustersListOfItemsPageListItem16" : MessageLookupByLibrary.simpleMessage("În momentul de față NU există nicio medicamentație recomandată care poate să prevină sau sa vindece coronavirusul"),
+    "whoMythBustersListOfItemsPageListItem2" : MessageLookupByLibrary.simpleMessage("Indivizii de toate vârstele POT fi infectați de coronavirus. Persoanele în vârstă și persoanele cu condiții preexistente (cum sunt astmul, diabetul sau boli de inimă) sunt presupune a fi mai vulnerabile la îmbolnăvirea gravă cu acest virus"),
+    "whoMythBustersListOfItemsPageListItem3" : MessageLookupByLibrary.simpleMessage("Vremea rece și zăpada nu pot omorî coronavirusul"),
+    "whoMythBustersListOfItemsPageListItem4" : MessageLookupByLibrary.simpleMessage("Coronavirusul POATE fi transmis în zone cu climă caldă și umedă"),
+    "whoMythBustersListOfItemsPageListItem5" : MessageLookupByLibrary.simpleMessage("Coronavirusul NU POATE fi transmis prin înțepătură de țânțar"),
+    "whoMythBustersListOfItemsPageListItem6" : MessageLookupByLibrary.simpleMessage("NU există nicio dovadă că animalele de companie ca pisicile sau câinii sunt capabile să transmită coronavirusul"),
+    "whoMythBustersListOfItemsPageListItem7" : MessageLookupByLibrary.simpleMessage("O baie fierbinte NU previne coronavirusul"),
+    "whoMythBustersListOfItemsPageListItem8" : MessageLookupByLibrary.simpleMessage("Uscătoarele cu aer cald NU sunt eficiente în omorârarea coronavirusului"),
+    "whoMythBustersListOfItemsPageListItem9" : MessageLookupByLibrary.simpleMessage("Lumina ultravioleta NU ar trebui folosită pentru dezinfecție și poate provoca iritații ale pielii")
+  };
+}
